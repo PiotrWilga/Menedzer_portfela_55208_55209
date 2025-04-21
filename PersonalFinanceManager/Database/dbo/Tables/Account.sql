94610f4cda10525d288025bf33c88fd4f0f50026
pawelwilga <pawel@wilga..net.pl>
2025-04-21 16:04:41 +0200

    Poprawki w schemacie bazy danych

--- PersonalFinanceManager/Database/dbo/Tables/Account.sql
+++ PersonalFinanceManager/Database/dbo/Tables/Account.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [dbo].[Account]
 (
-	[AccountId] INT NOT NULL,
+	[AccountId] INT NOT NULL IDENTITY,
 	[Name] NVARCHAR(200) NOT NULL,
 	[Type] SMALLINT,
-	[Currency] NVARCHAR(20) NOT NULL,
+	[CurrencyCode] NVARCHAR(20) NOT NULL,
 	[Balance] DECIMAL(38,4) NOT NULL,
 	[ShowInSummary] BIT,
 	CONSTRAINT [PK_Account] PRIMARY KEY CLUSTERED ([AccountId])
